ba54dbe0f02dc9e987a9049cd6f824aaf81a4da4
hamza.dev@pk.see.biz <hamza.dev@pk.see.biz>
2021-04-28 04:39:33 -0700

    categories are done for now

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,7 +1,28 @@
 const Categories = require("../models/categories")
 
-module.exports.getAll = (req,res,next)=>{
-    Categories.find().then(result=>{
+module.exports.getAll = (req, res, next) => {
+    Categories.find({}, { _id: 1, name: 1,productCount:1,
+        //  L1: 1, L2: 1, L3: 1, L4: 1,
+        parent:1 }).then(result => {
+        res.json(result)
+    })
+}
+
+module.exports.getByLevel = (req, res, next) => {
+    const Q = {
+        [`L${req.params.level}`]: { $ne: null },
+        [`L${Number(req.params.level) + 1}`]: null
+    }
+    Categories.find(Q).then(result => {
+        res.json(result)
+    })
+}
+
+module.exports.getByParent = (req, res, next) => {
+    const Q = {
+        parent: req.params.parentId
+    }
+    Categories.find(Q).then(result => {
         res.json(result)
     })
 }
--- routes/categories.js
+++ routes/categories.js
@@ -2,4 +2,6 @@ const categories = require("../controllers/categories");
 
 const router = require("express").Router();
 router.get("/",categories.getAll);
+router.get("/level/:level",categories.getByLevel);
+router.get("/parent/:parentId",categories.getByParent);
 module.exports = router;
