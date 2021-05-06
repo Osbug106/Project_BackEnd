9dfc1d53d17f6357decf24b3390ab4a4800bb72e
hamza.dev@pk.see.biz <hamza.dev@pk.see.biz>
2021-05-06 02:16:25 -0700

    products

--- controllers/products.js
+++ controllers/products.js
@@ -37,7 +37,7 @@ module.exports.getByCategoryInPages = (req, res) => {
                 if (r[getLevel(NUM_LEVELS)] === undefined)
                     getCategories(categories, r._id, getProducts, RCount);
                 else
-                    if(r._id)categories.push(mongoose.Types.ObjectId(r._id));
+                    if (r._id) categories.push(mongoose.Types.ObjectId(r._id));
             }
             categories.push(mongoose.Types.ObjectId(id));
         }).finally(() => {
@@ -135,11 +135,110 @@ module.exports.getByCategoryInPages = (req, res) => {
         })
     }
 }
+
+module.exports.getByCategory = (req, res) => {
+    const { category } = req.params;
+    (function getCategories(categories, id, getProducts, RCount) {
+        RCount = RCount || { count: 1 }
+        RCount.count++;
+        Categories.find({ parent: id }, { _id: 1, ...getAllLevels(NUM_LEVELS) }).then(result => {
+            RCount.count--;
+            for (let r of result) {
+                if (r[getLevel(NUM_LEVELS)] === undefined)
+                    getCategories(categories, r._id, getProducts, RCount);
+                else
+                    if (r._id) categories.push(mongoose.Types.ObjectId(r._id));
+            }
+            categories.push(mongoose.Types.ObjectId(id));
+        }).finally(() => {
+            if (RCount.count === 1) {
+                if (typeof getProducts === "function")
+                    getProducts(categories);
+            }
+        })
+    })([], category, getProducts);
+
+    function getProducts(categories) {
+        Products.count({ category: { $in: categories } }, (err, count) => {
+            if (err) {
+                res.json({ isOK: false, error: err })
+            } else {
+                Products.aggregate([
+                    {
+                        $match: {
+                            "category":
+                                { $in: categories }
+                        }
+                    },
+
+                    { $project: { title: 1, _id: 1, price: 1, product_images: { "250": 1 }, merchant_id: 1 } },
+                    {
+                        $lookup: {
+                            from: "users",
+                            // localField: "merchant_id",
+                            // foreignField: "_id",
+                            "let": { "id": "$merchant_id" },
+                            "pipeline": [
+                                {
+                                    "$match": {
+                                        "$expr": {
+                                            "$eq": ["$$id", "$_id"]
+                                        }
+                                    }
+                                },
+                                {
+                                    "$project": {
+                                        business_info: { businessName: 1 }, _id: 1, images: {
+                                            profile_pic: {
+                                                "250": 1
+                                            }
+                                        }
+                                    }
+                                }
+                            ],
+                            as: "merchant",
+                        },
+                    },
+                    { $unwind: '$merchant' },
+
+                    { $project: { title: 1, _id: 1, price: 1, product_images: { "250": 1 }, merchant: 1 } },
+
+                ])
+
+                    // Products
+                    //     .find({ category: { $in: categories } }, { merchant_id: 1, title: 1, _id: 1, product_images: { "250": 1 } })
+                    //     .limit(pageSize)
+                    //     .skip(pageSize * ($page - 1))
+                    //     .populate("merchant_id", {
+                    //         business_info: { businessName: 1 }, _id: 1, images: {
+                    //             profile_pic: {
+
+                    //                 "250": 1
+                    //             }
+                    //         }
+                    //     })
+                    .exec()
+                    .then((result) => {
+
+                        res.json({
+                            isOK: true,
+                            data: result,
+
+                        })
+                    })
+                    .catch(err => { res.json({ isOK: false, error: err }) })
+            }
+        })
+    }
+}
+
+
+
 module.exports.getByProductId = (req, res) => {
     Products.aggregate([
         {
             $match: {
-                _id:new mongoose.Types.ObjectId(req.params.id),
+                _id: new mongoose.Types.ObjectId(req.params.id),
             }
         }
         ,
@@ -186,4 +285,100 @@ module.exports.getByProductId = (req, res) => {
         })
         .catch(err => { res.json({ isOK: false, error: err.message }) })
 
+}
+
+
+
+module.exports.getByVendorInPages = (req, res) => {
+    const { vendor, page, size } = req.params;
+    let pageSize = PAGE_SIZE;
+    let $size = parseInt(size);
+    let $page = parseInt(page);
+    if (size && $size !== NaN) {
+        pageSize = $size
+    }
+    Products.count({ merchant_id: vendor }, (err, count) => {
+        if (err) {
+            res.json({ isOK: false, error: err })
+        } else {
+            Products.aggregate([
+                {
+                    $match: {
+                        "category":
+                            { $in: categories }
+                    }
+                },
+                {
+                    $skip: pageSize * ($page - 1)
+
+                },
+                {
+                    $limit: pageSize,
+                },
+                { $project: { title: 1, _id: 1, price: 1, product_images: { "250": 1 }, merchant_id: 1 } },
+                {
+                    $lookup: {
+                        from: "users",
+                        // localField: "merchant_id",
+                        // foreignField: "_id",
+                        "let": { "id": "$merchant_id" },
+                        "pipeline": [
+                            {
+                                "$match": {
+                                    "$expr": {
+                                        "$eq": ["$$id", "$_id"]
+                                    }
+                                }
+                            },
+                            {
+                                "$project": {
+                                    business_info: { businessName: 1 }, _id: 1, images: {
+                                        profile_pic: {
+                                            "250": 1
+                                        }
+                                    }
+                                }
+                            }
+                        ],
+                        as: "merchant",
+                    },
+                },
+                { $unwind: '$merchant' },
+
+                { $project: { title: 1, _id: 1, price: 1, product_images: { "250": 1 }, merchant: 1 } },
+
+            ])
+
+                // Products
+                //     .find({ category: { $in: categories } }, { merchant_id: 1, title: 1, _id: 1, product_images: { "250": 1 } })
+                //     .limit(pageSize)
+                //     .skip(pageSize * ($page - 1))
+                //     .populate("merchant_id", {
+                //         business_info: { businessName: 1 }, _id: 1, images: {
+                //             profile_pic: {
+
+                //                 "250": 1
+                //             }
+                //         }
+                //     })
+                .exec()
+                .then((result) => {
+                    let pages = Math.ceil(Number(count / pageSize));
+                    if (!pages && count > 0) {
+                        pages = 1;
+                    }
+                    res.json({
+                        isOK: true,
+                        data: result,
+                        meta: {
+                            totalPages: pages,
+                            size: pageSize,
+                            count: count,
+                            page: $page,
+                        },
+                    })
+                })
+                .catch(err => { res.json({ isOK: false, error: err }) })
+        }
+    })
 }
